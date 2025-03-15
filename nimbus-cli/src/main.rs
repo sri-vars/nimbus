7a535a24be1459ab580ee4b5d47c5335713fa564
srivarshan <me@srivarshan.dev>
2025-03-15 17:01:47 +0000

    feat(nimbus-cli): nimbus & nimbus-node subcommand added

--- nimbus-cli/src/main.rs
+++ nimbus-cli/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use nimbus_cli::{
+    Result,
+    args::{ArgRunner, NimbusArg},
+};
+
+fn main() -> Result<()> {
+    let arg: NimbusArg = argh::from_env();
+    arg.run()
 }
