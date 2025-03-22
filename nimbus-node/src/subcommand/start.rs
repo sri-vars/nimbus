5b77c1fc72b294a7670aa87a2a71e3ed7647f593
srivarshan <me@srivarshan.dev>
2025-03-22 21:27:53 +0530

    feat(transport): generic transport layer to expand functionality

--- nimbus-node/src/subcommand/start.rs
+++ nimbus-node/src/subcommand/start.rs
@@ -1,100 +1,136 @@
-use std::{collections::VecDeque, net::SocketAddr, path::PathBuf, sync::Arc};
+use std::{path::PathBuf, sync::Arc};
 
+use async_trait::async_trait;
 use nimbus_conf::NimbusClusterConfiguration;
-use nimbus_rt::{
-    Task,
-    futures::{AsyncReadExt, join},
-    io::AsyncWriteExt,
-    lock::Mutex,
-    net::TcpListener,
-    rt::NimbusRt,
-};
+use nimbus_rt::{Task, futures::join, rt::NimbusRt};
 
-use crate::{Error, Node, Result};
+use crate::{
+    Node, Result,
+    resource::transport::{TcpTransport, TransportStreamTrait, TransportTrait},
+};
 
 pub fn node_start(config: &PathBuf, node: u64) -> Result<()> {
     let nc_config = NimbusClusterConfiguration::new(config)?;
     let node = Arc::new(Node::new(nc_config, node)?);
 
     let rt = NimbusRt::instance();
-    rt.run(async {
-        let public_task = NimbusRt::spawn(public_task(node.clone()));
-        let private_task = NimbusRt::spawn(private_task(node));
+    rt.run(async move {
+        let node_config = node.node_config.read().await;
+        let public_transport = TcpTransport::new(node_config.public_transport.sock_addr())
+            .await
+            .unwrap();
+        let private_transport = TcpTransport::new(node_config.private_transport.sock_addr())
+            .await
+            .unwrap();
 
-        let (public_task_res, private_task_res) = join!(public_task, private_task);
+        let public_task = PublicNodeTask::new(node.clone(), public_transport).await;
+        let private_task = PrivateNodeTask::new(node.clone(), private_transport).await;
 
-        if let Err(e) = public_task_res {
-            eprintln!("Public task failed: {:?}", e);
-        }
-        if let Err(e) = private_task_res {
-            eprintln!("Private task failed: {:?}", e);
-        }
+        // Get the task objects
+        let public_task_handle = public_task.run();
+        let private_task_handle = private_task.run();
+
+        // Await the inner results separately
+        let (public_task_res, private_task_res) =
+            join!(public_task_handle.await, private_task_handle.await);
+
+        println!("{:#?}", public_task_res);
+        println!("{:#?}", private_task_res);
     });
     Ok(())
 }
 
-async fn public_task(node: Arc<Node>) -> Result<()> {
-    let node_config = node.node_config.read().await;
-    let sock_addr = node_config.public_transport.sock_addr();
-    echo_server(sock_addr).await
+pub struct PublicNodeTask<T: TransportTrait> {
+    pub node: Arc<Node>,
+    pub transport: Arc<T>,
 }
 
-async fn private_task(node: Arc<Node>) -> Result<()> {
-    let node_config = node.node_config.read().await;
-    let sock_addr = node_config.private_transport.sock_addr();
-    echo_server(sock_addr).await
+pub struct PrivateNodeTask<T: TransportTrait> {
+    pub node: Arc<Node>,
+    pub transport: Arc<T>,
 }
 
-async fn echo_server(addr: SocketAddr) -> Result<()> {
-    let listener = TcpListener::bind(&addr).await?;
-    println!("Listening on {}", addr);
-    let tasks = Arc::new(Mutex::new(VecDeque::<Task<Result<()>>>::new()));
-    let tasks_clone = tasks.clone();
-    let accept_task = NimbusRt::spawn(async move {
-        loop {
-            match listener.accept().await {
-                Ok((socket, peer_addr)) => {
-                    println!("New connection from {}", peer_addr);
-                    let handle = NimbusRt::spawn(handle_client(socket, peer_addr));
-                    let mut tasks_guard = tasks_clone.lock().await;
-                    tasks_guard.push_back(handle);
-                    while let Some(task) = tasks_guard.front() {
-                        if task.is_finished() {
-                            let task = tasks_guard.pop_front().unwrap();
-                            drop(tasks_guard);
-                            if let Err(e) = task.await {
-                                eprintln!("Task error: {:?}", e);
-                            }
-                            tasks_guard = tasks_clone.lock().await;
-                        } else {
-                            break;
-                        }
-                    }
-                }
-                Err(e) => {
-                    eprintln!("Error accepting connection: {:?}", e);
-                }
+impl<T: TransportTrait> PublicNodeTask<T> {
+    pub async fn new(node: Arc<Node>, transport: T) -> Self {
+        Self {
+            node,
+            transport: Arc::new(transport),
+        }
+    }
+}
+
+impl<T: TransportTrait> PrivateNodeTask<T> {
+    pub async fn new(node: Arc<Node>, transport: T) -> Self {
+        Self {
+            node,
+            transport: Arc::new(transport),
+        }
+    }
+}
+
+#[async_trait]
+pub trait NodeTaskTrait {
+    type TaskType;
+    async fn run(&self) -> Task<Self::TaskType>;
+}
+
+#[async_trait]
+impl<T: TransportTrait + Send + Sync + 'static> NodeTaskTrait for PublicNodeTask<T> {
+    type TaskType = Result<()>;
+
+    async fn run(&self) -> Task<Self::TaskType> {
+        let transport = self.transport.clone();
+        NimbusRt::spawn(async move { echo_server(transport).await })
+    }
+}
+
+#[async_trait]
+impl<T: TransportTrait + Send + Sync + 'static> NodeTaskTrait for PrivateNodeTask<T> {
+    type TaskType = Result<()>;
+
+    async fn run(&self) -> Task<Self::TaskType> {
+        let transport = self.transport.clone();
+        NimbusRt::spawn(async move { echo_server(transport).await })
+    }
+}
+
+async fn echo_server<T: TransportTrait + Send + Sync + 'static>(transport: Arc<T>) -> Result<()> {
+    let transport_clone = transport.clone();
+    let mut tasks = vec![];
+    loop {
+        match transport_clone.accept().await {
+            Ok(transport_stream) => {
+                println!("New connection from {}", transport_stream.peer_addr());
+                let transport_stream = Arc::new(transport_stream);
+                let task = NimbusRt::spawn(handle_client(transport_stream));
+                tasks.push(task);
+            }
+            Err(e) => {
+                eprintln!("Error accepting connection: {:?}", e);
             }
         }
-    });
-    accept_task.await;
-    Ok(())
+        tasks.retain(|task| !task.is_finished());
+    }
 }
-async fn handle_client(mut socket: nimbus_rt::net::TcpStream, peer_addr: SocketAddr) -> Result<()> {
+
+async fn handle_client<T: TransportStreamTrait + Send + Sync + 'static>(
+    transport_stream: Arc<T>,
+) -> Result<()> {
     let mut buffer = vec![0u8; 1024];
 
     loop {
-        let bytes_read = match socket.read(&mut buffer).await {
+        let bytes_read = match transport_stream.read(&mut buffer).await {
             Ok(0) => {
-                println!("Client {} disconnected", peer_addr);
+                println!("Client {} disconnected", transport_stream.peer_addr());
                 return Ok(());
             }
             Ok(n) => n,
-            Err(e) => return Err(Error::RuntimeIoError(e)),
+            Err(e) => return Err(e.into()),
         };
 
-        if let Err(e) = socket.write_all(&buffer[..bytes_read]).await {
-            eprintln!("Error writing to client {}: {:?}", peer_addr, e);
+        if let Err(e) = transport_stream.write(&buffer[..bytes_read]).await {
+            println!("Client {} disconnected", transport_stream.peer_addr());
+            return Err(e.into());
         }
     }
 }
